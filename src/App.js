8bf1b4b7e064f23b77d1a9672802bdc2222e3814
majkelmichel <51023578+majkelmichel@users.noreply.github.com>
2020-12-05 21:05:07 +0100

    Ex. 6.8 done

--- src/App.js
+++ src/App.js
@@ -1,30 +1,12 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { voteFor } from './reducers/anecdoteReducer';
 import AnecdoteForm from './components/AnecdoteForm';
+import AnecdoteList from './components/AnecdoteList';
 
 const App = () => {
-	const anecdotes = useSelector(state => state);
-	const dispatch = useDispatch();
-
-	const vote = (id) => {
-		dispatch(voteFor(id));
-	};
-
 	return (
 		<div>
 			<h2>Anecdotes</h2>
-			{anecdotes.sort((a,b) => a.votes < b.votes).map(anecdote =>
-				<div key={anecdote.id}>
-					<div>
-						{anecdote.content}
-					</div>
-					<div>
-						has {anecdote.votes}
-						<button onClick={() => vote(anecdote.id)}>vote</button>
-					</div>
-				</div>
-			)}
+			<AnecdoteList/>
 			<AnecdoteForm/>
 		</div>
 	);
